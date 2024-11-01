aabfa317eb2362409133a3e60df128d4fc8194a3
Tang am <tang.am@foxmail.com>
2024-11-02 01:19:05 +0800

    测试学习go

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/ammm56/lingscoin
+
+go 1.23.2
+
+require (
+	github.com/btcsuite/btcd v0.24.2
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4
+	github.com/btcsuite/btcd/btcutil v1.1.6
+	github.com/shirou/gopsutil v3.21.11+incompatible
+)
+
+require (
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/btcsuite/btclog v0.0.0-20241017175713-3428138b75c7 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
--- testing/study.go
+++ testing/study.go
@@ -1,6 +1,8 @@
 // Description: Go 语言学习测试代码
 // go mod tidy 生成 go.sum 文件
 // go mod init <module-name> 生成 go.mod 文件
+// go get <module-name> 下载依赖包
+// go get -u <module-name> 更新依赖包
 
 package main
 
@@ -8,7 +10,16 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
+
+	"github.com/shirou/gopsutil/mem"
 )
+
+var value int = 1
 
 func add(a int, b int) int {
 	return a + b
@@ -30,12 +41,25 @@ func operatingSystem() string {
 	fmt.Print("\t TotalAlloc: ", memstats.TotalAlloc/1024/1024, "MB")
 	fmt.Print("\t Sys: ", memstats.Sys/1024/1024, "MB")
 	fmt.Printf("\t NumGC: %v \n", memstats.NumGC)
+	// 获取系统内存
+	v, err := mem.VirtualMemory()
+	if err == nil {
+		fmt.Println("系统内存：", v.Total/1024/1024/1024, "GB")
+	}
 
 	return runtime.GOOS
 }
 
 // 测试函数
 func test1() {
+	// 变量和赋值
+	a0, b0 := 1, 2
+	value = add(a0, b0)
+	fmt.Println("a + b", "= ", value)
+	bitvalue := 1
+	fmt.Println("位运算左移 ", bitvalue<<1)
+	fmt.Println("位运算右移 ", bitvalue>>1)
+
 	// iota 常量生成器
 	const (
 		a = iota
@@ -76,14 +100,94 @@ func test1() {
 	}
 }
 
-var value int = 1
+func test2_btcutil() {
+	// 生成私钥
+	// SECP256K1
+	privKey1, err := btcec.NewPrivateKey()
+	if err != nil {
+		fmt.Println("生成私钥失败")
+		return
+	}
+	// 生成WIF格式的私钥
+	wif1, err := btcutil.NewWIF(privKey1, &chaincfg.MainNetParams, true)
+	if err != nil {
+		fmt.Println("生成WIF失败")
+		return
+	}
+	// 生成公钥 k * G = K
+	pubKey1 := privKey1.PubKey()
+	// P2PK Pay-to-PubKey
+	// 用公钥接收和锁定资金 验证交易 早期使用
+
+	// 生成P2PKH地址 Pay-to-PubKey-Hash 以 1 开头
+	// 对公钥进行两次哈希 第一次哈希使用 SHA256 第二次哈希使用 RIPEMD160，生成公钥哈希 Public Key Hash
+	// 在公钥哈希前加上版本号 0x00，生成版本化公钥哈希 Versioned Public Key Hash
+	// 对版本化公钥哈希进行两次哈希 第一次哈希使用 SHA256 第二次哈希使用 SHA256，生成校验码 Checksum
+	// 取校验码的前四个字节，加到版本化公钥哈希后面，生成校验版本化公钥哈希 Checksummed Versioned Public Key Hash
+	// 对校验版本化公钥哈希进行 Base58 编码，生成 P2PKH 地址
+	pubKeyHash1 := btcutil.Hash160(pubKey1.SerializeCompressed())
+	addressPKH, err := btcutil.NewAddressPubKeyHash(pubKeyHash1, &chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println("生成P2PKH地址失败")
+		return
+	}
+
+	// 生成P2SH地址 Pay-to-Script-Hash 以 3 开头
+	// 常见用途是多重签名钱包 需要多个私钥签名才能完成交易的地址
+	// 创建一个比特币脚本 常用OP_CHECKMULTISIG
+	// 对脚本进行RIPEMD160哈希生成脚本哈希 Script Hash
+	// 在脚本哈希前加上版本号 0x05，生成版本化脚本哈希 Versioned Script Hash
+	// 对版本化脚本哈希进行两次哈希 第一次哈希使用 SHA256 第二次哈希使用 SHA256，生成校验码 Checksum
+	// 取校验码的前四个字节，加到版本化脚本哈希后面，生成校验版本化脚本哈希 Checksummed Versioned Script Hash
+	// 对校验版本化脚本哈希进行 Base58 编码，生成 P2SH 地址
+	privKey2, err := btcec.NewPrivateKey()
+	if err != nil {
+		fmt.Println("生成私钥失败")
+		return
+	}
+	// 生成多重签名脚本
+	pubKey2 := []*btcec.PublicKey{privKey1.PubKey(), privKey2.PubKey()}
+	var addressPubKeys []*btcutil.AddressPubKey
+	for _, pubKey := range pubKey2 {
+		addressPubKey, err := btcutil.NewAddressPubKey(pubKey.SerializeCompressed(), &chaincfg.MainNetParams)
+		if err != nil {
+			fmt.Println("生成地址公钥失败")
+			return
+		}
+		addressPubKeys = append(addressPubKeys, addressPubKey)
+	}
+	script, err := txscript.MultiSigScript(addressPubKeys, 2)
+	if err != nil {
+		fmt.Println("生成多重签名脚本失败")
+		return
+	}
+	// 生成P2SH地址
+	addressP2SH, err := btcutil.NewAddressScriptHash(script, &chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println("生成P2SH地址失败")
+		return
+	}
+	// 生成Bech32地址 bc1 开头 P2WPKH地址 Pay-to-Witness-PubKey-Hash
+	// Hash160 里面做了两次哈希 一次是 SHA256 一次是 RIPEMD160
+	pubKeyHash := btcutil.Hash160(pubKey1.SerializeCompressed())
+	addressBech32, err := btcutil.NewAddressWitnessPubKeyHash(pubKeyHash, &chaincfg.MainNetParams)
+	if err != nil {
+		fmt.Println("生成Bech32地址失败")
+	}
+
+	fmt.Println("私钥: ", privKey1.Serialize())
+	fmt.Printf("私钥wif: %s \n", wif1.String())
+	fmt.Printf("未压缩公钥： %x \n", pubKey1.SerializeUncompressed())
+	fmt.Printf("压缩公钥： %x \n", pubKey1.SerializeCompressed())
+	fmt.Println("P2PKH地址: ", addressPKH.EncodeAddress())
+	fmt.Println("P2SH地址: ", addressP2SH.EncodeAddress())
+	fmt.Println("Bech32地址: ", addressBech32.EncodeAddress())
+
+}
 
 func main() {
-	a, b := 1, 2
-	value = add(a, b)
-	fmt.Println("a + b", "= ", value)
-	bitvalue := 1
-	fmt.Println("位运算左移 ", bitvalue<<1)
-	fmt.Println("位运算右移 ", bitvalue>>1)
 	test1()
+
+	test2_btcutil()
+
 }
